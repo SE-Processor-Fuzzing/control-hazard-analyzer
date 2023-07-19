f763f9337307a2710d7fb87e5e7e32bc9dc617b8
RoketFlame <72881638+RoketFlame@users.noreply.github.com>
2023-07-19 13:47:37 +0300

    feat: modify Builder, now it compiles all files in source folder

--- builders/builder.py
+++ builders/builder.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from typing import Dict
 
@@ -6,18 +7,16 @@ class Builder:
     def __init__(self, settings: Dict):
         self.settings = settings
 
-    def build(self):
-        # print(os.name)
-        # print(os.getcwd())
+    def build(self, src_folder: str, destination_folder: str):
+        list_of_src_files = os.listdir(src_folder)
+        print(list_of_src_files)
+        for test_file in list_of_src_files:
+            execute_line = [self.settings["compiler"], f"{src_folder}/{test_file}"] + self.settings["compiler_args"] + \
+                           ["-o", f"{destination_folder}/{test_file}.out"]
 
-        output_file = f"{self.settings['dest_folder']}/a.out"
+            print(" ".join(execute_line))
 
-        execute_line = [self.settings["compiler"], self.settings["test_file"]] + self.settings["compiler_args"] + \
-                       ["-o", f"{self.settings['dest_folder']}/{self.settings['compiled_folder']}/a.out"]
-
-        print(" ".join(execute_line))
-
-        subprocess.run(
-            execute_line,
-            check=True,
-        )
+            subprocess.run(
+                execute_line,
+                check=True,
+            )
--- src/controller.py
+++ src/controller.py
@@ -1,10 +1,12 @@
 from builders.builder import Builder
 from src.configurator import Configurator
+from src.generator import Generator
 
 
 class Controller:
 
     def __init__(self):
+        self.generator = None
         self.builder = None
         self.configurator = Configurator()
         self.glb_settings = {
@@ -26,11 +28,14 @@ class Controller:
         self.configurator.create_folders(self.glb_settings)
 
     def generate_test(self):
-        pass
+        self.generator = Generator(self.glb_settings)
+        self.generator.generate(self.glb_settings["repeats"])
 
     def build(self):
         self.builder = Builder(self.glb_settings)
-        self.builder.build()
+        source_folder = f'{self.glb_settings["dest_folder"]}/{self.glb_settings["source_folder"]}'
+        destination_folder = f'{self.glb_settings["dest_folder"]}/{self.glb_settings["compiled_folder"]}'
+        self.builder.build(source_folder, destination_folder)
 
     def profile(self):
         pass
