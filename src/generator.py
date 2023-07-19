da0e1a4f57bc9dae721658404f9334a46e7331b1
RoketFlame <72881638+RoketFlame@users.noreply.github.com>
2023-07-19 13:46:39 +0300

    feat: add simple code in Generator, which copy source file on several occasions

--- src/generator.py
+++ src/generator.py
@@ -1,2 +1,12 @@
+import shutil
+
+
 class Generator:
-    pass
+    def __init__(self, settings: dict):
+        self.settings = settings
+
+    def generate(self, repeats=1, output_filename_template="test"):
+        for i in range(repeats):
+            shutil.copy("test.c",
+                        f'{self.settings["dest_folder"]}/'
+                        f'{self.settings["source_folder"]}/{output_filename_template}_{i}.c')
