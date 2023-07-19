18a844effec9976e3a24f13fd86a2f65d08fa806
Osogi <dragonmail135159@gmail.com>
2023-07-19 23:00:54 +0300

    feat(perfProfiler): add builder to perfProfiler

--- profilers/perfProfiler.py
+++ profilers/perfProfiler.py
@@ -3,12 +3,12 @@ from pathlib import Path
 from profile import Profile
 from tempfile import mkdtemp
 import glob
+from builders.builder import Builder
 
 
 class PerfProfiler(Profile):
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, builder: Builder):
+        self.builder: Builder = builder
         self.temp_dir: Path = Path(mkdtemp())
         with open(Path("profilers/perfProfiler/template.c"), "rt") as reader:
             strings = reader.read().split("\n")
@@ -26,6 +26,12 @@ class PerfProfiler(Profile):
         dst_dir.mkdir(parents=True, exist_ok=True)
         for src_test in glob.glob(str(src_dir) + "/*.c"):
             src_test = Path(src_test)
-            self.patch_test(src_test, self.temp_dir.joinpath(src_test.name))
+            self.patch_test(src_test, dst_dir.joinpath(src_test.name))
             return True
         return False
+
+    def profile(self, test_dir: Path, analyze_dir: Path):
+        src_dir = self.temp_dir.joinpath("src/")
+        build_dir = self.temp_dir.joinpath("build/")
+        self.patch_tests_in_dir(test_dir, src_dir)
+        self.builder.build(src_dir, build_dir)
