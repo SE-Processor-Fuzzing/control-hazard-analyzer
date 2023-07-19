5c0bd6bd01e3584268b91fa3ff44f6fce82470b7
Osogi <dragonmail135159@gmail.com>
2023-07-19 23:00:54 +0300

    fix: fix some bugs + black auto-format

--- src/controller.py
+++ src/controller.py
@@ -1,22 +1,22 @@
+from pathlib import Path
 from builders.builder import Builder
 from src.configurator import Configurator
 from src.generator import Generator
 
 
 class Controller:
-
     def __init__(self):
-        self.generator = None
-        self.builder = None
         self.configurator = Configurator()
         self.glb_settings = {
             "compiled_folder": "compiled",
             "source_folder": "src",
             "analyse_folder": "analyse",
         }
+        self.configurate()
+        self.builder: Builder = Builder(self.glb_settings)
+        self.generator = Generator(self.glb_settings)
 
     def run(self):
-        self.configurate()
         self.generate_test()
         self.build()
         self.profile()
@@ -24,18 +24,21 @@ class Controller:
 
     def configurate(self):
         self.glb_settings = self.configurator.configurate(self.glb_settings)
-        self.configurator.clean_output_folder(self.glb_settings["dest_folder"])
+        self.configurator.clean_output_folder(Path(self.glb_settings["dest_folder"]))
         self.configurator.create_folders(self.glb_settings)
 
     def generate_test(self):
-        self.generator = Generator(self.glb_settings)
         self.generator.generate(self.glb_settings["repeats"])
 
     def build(self):
         self.builder = Builder(self.glb_settings)
-        source_folder = f'{self.glb_settings["dest_folder"]}/{self.glb_settings["source_folder"]}'
-        destination_folder = f'{self.glb_settings["dest_folder"]}/{self.glb_settings["compiled_folder"]}'
-        self.builder.build(source_folder, destination_folder)
+        source_folder = (
+            f'{self.glb_settings["dest_folder"]}/{self.glb_settings["source_folder"]}'
+        )
+        destination_folder = (
+            f'{self.glb_settings["dest_folder"]}/{self.glb_settings["compiled_folder"]}'
+        )
+        self.builder.build(Path(source_folder), Path(destination_folder))
 
     def profile(self):
         pass
