11327bd5e23c3b1ec3067fa691dccebbb3dad4da
Osogi <dragonmail135159@gmail.com>
2023-07-21 17:46:18 +0300

    feat: add Packer

--- src/packer.py
+++ src/packer.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict, Protocol
 from pathlib import Path
 
@@ -5,3 +6,11 @@ from pathlib import Path
 class IPacker(Protocol):
     def pack(self, out_dir: Path, analyzed_data: Dict[str, Dict]):
         ...
+
+
+class Packer:
+    def pack(self, out_dir: Path, analyzed_data: Dict[str, Dict]):
+        out_dir.mkdir(parents=True, exist_ok=True)
+        for key in analyzed_data:
+            with open(out_dir.joinpath(key + ".data"), "wt") as writter:
+                writter.write(json.dumps(analyzed_data[key]))
