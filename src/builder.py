8e8b3dd76253f09ff2952bf3c9bd2d5179439e68
Osogi <dragonmail135159@gmail.com>
2023-07-24 16:58:21 +0300

    fix: fix builder.py to new arch

--- src/builder.py
+++ src/builder.py
@@ -1,22 +1,21 @@
+from argparse import Namespace
 import os
 from pathlib import Path
 import subprocess
-from typing import Dict
 
 
 class Builder:
-    def __init__(self, settings: Dict):
+    def __init__(self, settings: Namespace):
         self.settings = settings
 
     def build(self, src_folder: Path, destination_folder: Path):
         list_of_src_files = os.listdir(src_folder)
         destination_folder.mkdir(parents=True, exist_ok=True)
-        print(list_of_src_files)
         for test_file in list_of_src_files:
             execute_line = (
-                [self.settings["compiler"], f"{src_folder}/{test_file}"]
-                + self.settings["compiler_args"]
-                + ["-o", f"{destination_folder}/{test_file}.out"]
+                [self.settings.compiler, src_folder.joinpath(test_file)]
+                + self.settings.compiler_args
+                + ["-o", destination_folder.joinpath(f"{test_file}.out")]
             )
 
             print(" ".join(execute_line))
