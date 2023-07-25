bf7dcf7be216a3dce890d79f1fd468b550dab335
xJoskiy <pavel5553-20@mail.ru>
2023-07-25 18:34:16 +0300

    feat: add building and running options for gem5

--- src/builder.py
+++ src/builder.py
@@ -8,7 +8,7 @@ class Builder:
     def __init__(self, settings: Namespace):
         self.settings = settings
 
-    def build(self, src_folder: Path, destination_folder: Path):
+    def build(self, src_folder: Path, destination_folder: Path, additional_flags: list[str] = []):
         list_of_src_files = os.listdir(src_folder)
         destination_folder.mkdir(parents=True, exist_ok=True)
         for test_file in list_of_src_files:
@@ -16,11 +16,10 @@ class Builder:
                 [self.settings.compiler, src_folder.joinpath(test_file)]
                 + self.settings.compiler_args
                 + ["-o", destination_folder.joinpath(f"{test_file}.out")]
+                + additional_flags
             )
-
-            # print(" ".join(map(lambda x: str(x), execute_line)))
 
             subprocess.run(
                 execute_line,
-                check=True,
+                check=True
             )
