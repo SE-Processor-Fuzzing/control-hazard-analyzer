a32ed9e226fb6ba991a9042925ef5c8f531680a3
Osogi <dragonmail135159@gmail.com>
2023-07-26 14:35:10 +0300

    fix(perfProfiler): edit perfTemplate

--- profilers/attachments/perfTemplate.c
+++ profilers/attachments/perfTemplate.c
@@ -9,12 +9,6 @@
 
 typedef void (*traced_fun_t)();
 
-typedef struct _perf_branch_data_t {
-    unsigned long long branches;
-    unsigned long long missed_branches;
-    unsigned long long bpu;
-} perf_branch_data_t;
-
 long perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int group_fd, unsigned long flags) {
     int ret;
 
@@ -22,48 +16,61 @@ long perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int g
     return ret;
 }
 
-unsigned long long traceFunction(traced_fun_t traced, unsigned long long config) {
-    struct perf_event_attr pe;
-    long long count;
+int set_up_perf_event(struct perf_event_attr* pe, unsigned long long config) {
     int fd;
+    memset(pe, 0, sizeof(struct perf_event_attr));
+    pe->type = PERF_TYPE_HARDWARE;
+    pe->size = sizeof(struct perf_event_attr);
+    pe->config = config;
+    pe->disabled = 1;
+    pe->exclude_kernel = 1;
+    pe->exclude_hv = 1;
 
-    memset(&pe, 0, sizeof(struct perf_event_attr));
-    pe.type = PERF_TYPE_HARDWARE;
-    pe.size = sizeof(struct perf_event_attr);
-    pe.config = config;
-    pe.disabled = 1;
-    pe.exclude_kernel = 1;
-    pe.exclude_hv = 1;
-
-    fd = perf_event_open(&pe, 0, -1, -1, 0);
+    fd = perf_event_open(pe, 0, -1, -1, 0);
     if (fd == -1) {
-        fprintf(stderr, "Error opening leader %llx\n", pe.config);
+        fprintf(stderr, "Error opening leader %llx\n", pe->config);
         exit(EXIT_FAILURE);
     }
+    return fd;
+}
 
-    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+unsigned long long* traceFunction(traced_fun_t traced, unsigned long long* configs, int configs_len) {
+    struct perf_event_attr pe[configs_len];
+    unsigned long long* results = calloc(configs_len, sizeof(*results));
+    int fd[configs_len];
+    for (int i = 0; i < configs_len; i++) {
+        fd[i] = set_up_perf_event(&pe[i], configs[i]);
+    }
+
+    for (int i = 0; i < configs_len; i++) {
+        ioctl(fd[i], PERF_EVENT_IOC_RESET, 0);
+    }
+
+    for (int i = 0; i < configs_len; i++) {
+        ioctl(fd[i], PERF_EVENT_IOC_ENABLE, 0);
+    }
 
     traced();
 
-    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-    read(fd, &count, sizeof(long long));
-    close(fd);
+    for (int i = 0; i < configs_len; i++) {
+        ioctl(fd[i], PERF_EVENT_IOC_DISABLE, 0);
+    }
 
-    return count;
-}
+    for (int i = 0; i < configs_len; i++) {
+        read(fd[i], &results[i], sizeof(long long));
+        close(fd[i]);
+    }
 
-perf_branch_data_t fullTraceFunction(traced_fun_t traced) {
-    perf_branch_data_t branch_data;
-    branch_data.branches = traceFunction(traced, PERF_COUNT_HW_BRANCH_INSTRUCTIONS);
-    branch_data.missed_branches = traceFunction(traced, PERF_COUNT_HW_BRANCH_MISSES);
-    branch_data.bpu = traceFunction(traced, PERF_COUNT_HW_CACHE_BPU);
-    return branch_data;
+    return results;
 }
 
 int main() {
-    perf_branch_data_t res = fullTraceFunction(test_fun);
-    printf("branches: %llu\n", res.branches);
-    printf("missed_branches: %llu\n", res.missed_branches);
-    printf("cache_BPU: %llu\n", res.bpu);
+    unsigned long long configs[] = {PERF_COUNT_HW_BRANCH_INSTRUCTIONS, PERF_COUNT_HW_BRANCH_MISSES,
+                                    PERF_COUNT_HW_CACHE_BPU, PERF_COUNT_SW_CPU_CLOCK, PERF_COUNT_HW_INSTRUCTIONS};
+    unsigned long long* data = traceFunction(test_fun, configs, sizeof(configs) / sizeof(*configs));
+    printf("branches: %llu\n", data[0]);
+    printf("missed_branches: %llu\n", data[1]);
+    printf("cache_BPU: %llu\n", data[2]);
+    printf("cpu_clock: %llu\n", data[3]);
+    printf("instructions: %llu\n", data[4]);
 }
