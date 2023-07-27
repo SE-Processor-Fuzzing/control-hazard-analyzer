d35f73f5c39636ba60f78915e50c9b341ce58442
Osogi <dragonmail135159@gmail.com>
2023-07-27 21:52:07 +0300

    fix: edit out dir generate

--- src/generate.py
+++ src/generate.py
@@ -11,7 +11,7 @@ class Generator:
         self.repeats: Optional[int] = None
 
     def configurate(self, settings: Namespace) -> None:
-        self.src_dir: Path = Path(settings.dest_folder).joinpath("src")
+        self.out_dir: Path = Path(settings.out_dir)
         self.repeats = int(settings.repeats)
 
     def generate_tests(self, target_dir: Path, count: int, verbose: bool = False):
@@ -27,12 +27,12 @@ class Generator:
         dir.mkdir(parents=True)
 
     def run(self) -> None:
-        self.create_empty_dir(self.src_dir)
-        self.generate_tests(self.src_dir, self.repeats, verbose=True)
+        self.create_empty_dir(self.out_dir)
+        self.generate_tests(self.out_dir, self.repeats, verbose=True)
 
     def add_sub_parser(self, sub_parser) -> ArgumentParser:
         self.analyze_parser: ArgumentParser = sub_parser.add_parser("generate", prog="generate")
-        self.analyze_parser.add_argument("--dest_folder", default="out", help="Path to output folder")
+        self.analyze_parser.add_argument("--out_dir", default="tests", help="Path to output folder")
         self.analyze_parser.add_argument("--repeats", type=int, default=1, help="Count of repeats to generated test")
         return self.analyze_parser
 
