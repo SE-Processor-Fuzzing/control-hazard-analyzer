b0e412610b07cd6ae78e5a8793e1c36f914f6c95
[TheSimBiOne] <[thesimbione@gmail.com]>
2023-07-28 00:41:48 +0300

    feat: add c code generation class

--- src/generate.py
+++ src/generate.py
@@ -2,6 +2,7 @@ import shutil
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import List, Optional
+import blocks_rendering as br
 
 
 class Generator:
@@ -14,12 +15,12 @@ class Generator:
         self.out_dir: Path = Path(settings.out_dir)
         self.repeats = int(settings.repeats)
 
-    def generate_tests(self, target_dir: Path, count: int, verbose: bool = False):
-        if verbose:
+    def generate_tests(self, target_dir: Path, count: int, total_blocks: int, blocks_cap: int, vars_cap: int, verbose: bool = False):
+        if (verbose):
             print(f"Generate tests to '{target_dir.absolute()}'")
-
         for i in range(count):
-            shutil.copy("test.c", target_dir.joinpath(f"test_{i}.c"))
+            br.generate_test(target_dir.joinpath(f'test_{i}.c'), total_blocks, blocks_cap, vars_cap)
+
 
     def create_empty_dir(self, dir: Path):
         if dir.exists():
@@ -28,7 +29,7 @@ class Generator:
 
     def run(self) -> None:
         self.create_empty_dir(self.out_dir)
-        self.generate_tests(self.out_dir, self.repeats, verbose=True)
+        self.generate_tests(self.src_dir, self.repeats, verbose=True)
 
     def add_sub_parser(self, sub_parser) -> ArgumentParser:
         self.analyze_parser: ArgumentParser = sub_parser.add_parser("generate", prog="generate")
