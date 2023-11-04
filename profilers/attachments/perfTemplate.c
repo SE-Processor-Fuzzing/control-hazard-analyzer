7024971f36ce258cc38a3f665d6cf5301533a514
Osogi <dragonmail135159@gmail.com>
2023-11-04 16:21:28 +0300

    feat: rework perf template

--- profilers/attachments/perfTemplate.c
+++ profilers/attachments/perfTemplate.c
@@ -1,13 +1,17 @@
-// #include "/full/path/to/test/file"
 #include <asm/unistd.h>
 #include <linux/perf_event.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-typedef void (*traced_fun_t)();
+int* perf_fd;
+size_t perf_fd_len;
+unsigned long long configs[] = {PERF_COUNT_HW_BRANCH_INSTRUCTIONS, PERF_COUNT_HW_BRANCH_MISSES, PERF_COUNT_HW_CACHE_BPU,
+                                PERF_COUNT_SW_CPU_CLOCK, PERF_COUNT_HW_INSTRUCTIONS};
+char* value_names[] = {"branches", "missed_branches", "cache_BPU", "cpu_clock", "instructions"};
 
 long perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int group_fd, unsigned long flags) {
     int ret;
@@ -16,9 +20,9 @@ long perf_event_open(struct perf_event_attr* hw_event, pid_t pid, int cpu, int g
     return ret;
 }
 
-int set_up_perf_event(struct perf_event_attr* pe, unsigned long long config) {
+int set_up_perf_event(unsigned long long config) {
     int fd;
-    memset(pe, 0, sizeof(struct perf_event_attr));
+    struct perf_event_attr* pe = malloc(sizeof(struct perf_event_attr));
     pe->type = PERF_TYPE_HARDWARE;
     pe->size = sizeof(struct perf_event_attr);
     pe->config = config;
@@ -29,48 +33,55 @@ int set_up_perf_event(struct perf_event_attr* pe, unsigned long long config) {
     fd = perf_event_open(pe, 0, -1, -1, 0);
     if (fd == -1) {
         fprintf(stderr, "Error opening leader %llx\n", pe->config);
-        exit(EXIT_FAILURE);
+        // exit(EXIT_FAILURE);
     }
+    free(pe);
     return fd;
 }
 
-unsigned long long* traceFunction(traced_fun_t traced, unsigned long long* configs, int configs_len) {
-    struct perf_event_attr pe[configs_len];
-    unsigned long long* results = calloc(configs_len, sizeof(*results));
-    int fd[configs_len];
+static void init() {
+    size_t configs_len = sizeof(configs) / sizeof(*configs);
+    perf_fd_len = configs_len;
+    perf_fd = calloc(configs_len, sizeof(*perf_fd));
     for (int i = 0; i < configs_len; i++) {
-        fd[i] = set_up_perf_event(&pe[i], configs[i]);
+        perf_fd[i] = set_up_perf_event(configs[i]);
     }
 
     for (int i = 0; i < configs_len; i++) {
-        ioctl(fd[i], PERF_EVENT_IOC_RESET, 0);
+        if (perf_fd[i] != -1)
+            ioctl(perf_fd[i], PERF_EVENT_IOC_RESET, 0);
     }
 
     for (int i = 0; i < configs_len; i++) {
-        ioctl(fd[i], PERF_EVENT_IOC_ENABLE, 0);
+        if (perf_fd[i] != -1)
+            ioctl(perf_fd[i], PERF_EVENT_IOC_ENABLE, 0);
     }
+}
 
-    traced();
+static void fin() {
+    if (perf_fd != NULL) {
+        for (int i = 0; i < perf_fd_len; i++) {
+            if (perf_fd[i] != -1)
+                ioctl(perf_fd[i], PERF_EVENT_IOC_DISABLE, 0);
+        }
 
-    for (int i = 0; i < configs_len; i++) {
-        ioctl(fd[i], PERF_EVENT_IOC_DISABLE, 0);
+        long long value_result = -1;
+        for (int i = 0; i < perf_fd_len; i++) {
+            if (perf_fd[i] != -1) {
+                read(perf_fd[i], &value_result, sizeof(long long));
+                close(perf_fd[i]);
+            } else {
+                value_result = -1;
+            }
+            printf("%s: %lld\n", value_names[i], value_result);
+        }
     }
-
-    for (int i = 0; i < configs_len; i++) {
-        read(fd[i], &results[i], sizeof(long long));
-        close(fd[i]);
-    }
-
-    return results;
+    exit(0);
 }
 
 int main() {
-    unsigned long long configs[] = {PERF_COUNT_HW_BRANCH_INSTRUCTIONS, PERF_COUNT_HW_BRANCH_MISSES,
-                                    PERF_COUNT_HW_CACHE_BPU, PERF_COUNT_SW_CPU_CLOCK, PERF_COUNT_HW_INSTRUCTIONS};
-    unsigned long long* data = traceFunction(test_fun, configs, sizeof(configs) / sizeof(*configs));
-    printf("branches: %llu\n", data[0]);
-    printf("missed_branches: %llu\n", data[1]);
-    printf("cache_BPU: %llu\n", data[2]);
-    printf("cpu_clock: %llu\n", data[3]);
-    printf("instructions: %llu\n", data[4]);
+    signal(SIGINT, fin);
+    init();
+    test_fun();
+    fin();
 }
