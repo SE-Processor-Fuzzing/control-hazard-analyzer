6fd35b536ec6ae89778202174e568c8f9e3813cc
Danil S <danil.slinchuk@gmail.com>
2023-11-07 18:33:37 +0300

    style: format code with black

--- src/builder.py
+++ src/builder.py
@@ -10,21 +10,20 @@ class Builder:
 
     def build(
         self,
-        src_folder: Path,
-        destination_folder: Path,
+        src_dir: Path,
+        destination_dir: Path,
         additional_flags: list[str] = [],
     ):
         list_of_src_files = os.listdir(src_folder)
         destination_folder.mkdir(parents=True, exist_ok=True)
         for test_file in list_of_src_files:
             execute_line = (
-                [self.settings.compiler, src_folder.joinpath(test_file)]
+                [self.settings.compiler, src_dir.joinpath(test_file)]
                 + self.settings.compiler_args
-                + ["-o", destination_folder.joinpath(f"{test_file}.out")]
+                + ["-o", destination_dir.joinpath(f"{test_file}.out")]
                 + additional_flags
             )
             if self.settings.debug:
-                print("Builder(Analyze) is running. Executed command:")
-                print(execute_line)
+                print(f"Builder(Analyze) is running. Executed command:{execute_line}")
 
             subprocess.run(execute_line, check=True)
