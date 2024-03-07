c3701e15ffaf93a5d039cd666dd376bc7dbe9737
Osogi <dragonmail135159@gmail.com>
2024-03-07 19:13:03 +0300

    fix: fix timeout return code

--- src/analyzers/collectors/sshCollector.py
+++ src/analyzers/collectors/sshCollector.py
@@ -80,7 +80,9 @@ class SshCollector:
 
     def execute_test(self, execute_line: List[str], timeout: float) -> TestRes:
         execute_str = " ".join(execute_line)
-        chan = self.execute_command(f"timeout -s SIGINT {timeout}s {execute_str}")
+
+        # TODO: sometime timeout don't work and programm hangs
+        chan = self.execute_command(f"timeout --preserve-status -s SIGINT {timeout}s {execute_str}")
 
         is_full = True
         ret_code = chan.recv_exit_status()
