1fe29103368371e6a36a4a19a4887336060ce886
Danil S <danil.slinchuk@gmail.com>
2024-03-07 13:02:12 +0300

    refactor: rectify PR remarks

--- src/generators/code_gen.py
+++ src/generators/code_gen.py
@@ -9,40 +9,46 @@ K_T = TypeVar("K_T")
 
 
 def get_random_key(dct: Dict[K_T, int]) -> K_T:
-    """!
-    @brief Get random key from dict where values is weights
+    """
+    Get random key from dict where values is weights.
 
-    @param dct Dict[K_T, int]
-
-    @return K_T - key from dict
+    :param dct: A used dict.
+    :return: Key from dct.
     """
     return rd.choices(list(dct.keys()), weights=list(dct.values()))[0]
 
 
-# Example class Visitor
-class Visitor:
-    def __init__(self) -> None:
-        pass
+class Visitor(Protocol):
+    def __init__(self) -> None: ...
 
-    def send(self, string: str) -> None:
-        print(string)
+    def send(self, string: str) -> None: ...
 
 
 @dataclass
 class Probabilities:
-    blocks_chanses: Dict[Callable[["Environment"], "Block"], int]
+    blocks_chanses: Dict[Callable[["Scope"], "Block"], int]
     operators_chanses: Dict[str, int]
     conditions_chanses: Dict[str, int]
     blocks_cut: Tuple[int, int]
 
 
-class Environment:
-    def __init__(self) -> None:
-        self.bin_operators = ["+", "-", "/", "*"]
-        self.condition_operators = [">", "<", ">=", "<=", "==", "!="]
+class Scope:
+
+    def __init__(
+        self,
+        blocks_count: int,
+        bin_operators: Optional[List[str]] = None,
+        condition_operators: Optional[List[str]] = None,
+    ) -> None:
+        if bin_operators is None:
+            bin_operators = ["+", "-", "/", "*"]
+        if condition_operators is None:
+            condition_operators = [">", "<", ">=", "<=", "==", "!="]
+        self.bin_operators = bin_operators
+        self.condition_operators = condition_operators
+        self.blocks_count = blocks_count
         self.vars: List[str] = []
         self.vars_count = 0
-        self.blocks_count = 10
 
     def create_new_var(self, prefix: str = "a") -> str:
         self.vars.append(var := f"{prefix}{self.vars_count}")
@@ -55,12 +61,12 @@ class Environment:
         return rd.choice(list(filter(rule, self.vars)))
 
     def get_random_vars(self, rule: Optional[Callable[[str], bool]] = None, count: int = 1) -> List[str]:
-        vars = self.vars
+        _vars = self.vars
         if rule is not None:
-            vars = list(filter(rule, vars))
-        if count > len(vars):
-            return rd.choices(vars, k=count)
-        return rd.sample(vars, k=count)
+            _vars = list(filter(rule, _vars))
+        if count > len(_vars):
+            return rd.choices(_vars, k=count)
+        return rd.sample(_vars, k=count)
 
     def copy(self) -> Self:
         c = copy(self)
@@ -69,38 +75,25 @@ class Environment:
 
 
 class Block(Protocol):
-    def __init__(self, env: Environment, probs: Probabilities) -> None: ...
+    def __init__(self, env: Scope, probs: Probabilities) -> None: ...
 
     def render(self, visitor: Visitor) -> None: ...
 
 
-class ForBlock(Block):
+class DefineBlock(Block):
     max_value = 100
 
-    def __init__(self, env: Environment, _: Probabilities) -> None:
-        self.def_var = env.create_new_var(prefix="f")
-        self.condition = Condition(self.def_var, str(rd.randint(2, ForBlock.max_value)), "<=")
-        self.next_blocks: List[Block] = []
+    def __init__(self, env: Scope, _: Probabilities) -> None:
+        self.var = env.create_new_var()
+        self.value = rd.randint(0, DefineBlock.max_value)
 
     def render(self, visitor: Visitor) -> None:
-        string = f"for (int {self.def_var} = 0; {self.condition.render()}; {self.def_var}++) {{"
+        string = f"int {self.var} = {self.value};"
         visitor.send(string)
-        for block in self.next_blocks:
-            block.render(visitor)
-        visitor.send("}")
-
-
-class Condition:
-    def __init__(self, lvar: str, rvar: str, operator: str) -> None:
-        self.lvar, self.rvar = lvar, rvar
-        self.operator = operator
-
-    def render(self) -> str:
-        return f"{self.lvar} {self.operator} {self.rvar}"
 
 
 class OperationBlock(Block):
-    def __init__(self, env: Environment, probs: Probabilities) -> None:
+    def __init__(self, env: Scope, probs: Probabilities) -> None:
         rule: Callable[[str], bool] = lambda x: x.startswith("a")
         self.env = env
         self.cur_var = env.get_random_var(rule=rule)
@@ -109,12 +102,22 @@ class OperationBlock(Block):
 
     def render(self, visitor: Visitor) -> None:
         self.var_2 = f"(D0({self.var_2}))" if self.operator == "/" else self.var_2
-        string = f"{self.cur_var} = {self.var_1} {self.operator} {self.var_2}; // {self.env.vars}"
+        string = f"{self.cur_var} = {self.var_1} {self.operator} {self.var_2};"
         visitor.send(string)
 
 
+@dataclass
+class Condition:
+    lvar: str
+    rvar: str
+    operator: str
+
+    def render(self) -> str:
+        return f"{self.lvar} {self.operator} {self.rvar}"
+
+
 class IfConditionBlock(Block):
-    def __init__(self, env: Environment, probs: Probabilities) -> None:
+    def __init__(self, env: Scope, probs: Probabilities) -> None:
         lvar, rvar = env.get_random_vars(count=2)
         self.condition = Condition(lvar, rvar, get_random_key(probs.conditions_chanses))
         self.then_blocks: List[Block] = []
@@ -131,20 +134,24 @@ class IfConditionBlock(Block):
         visitor.send("}")
 
 
-class DefineBlock(Block):
+class ForBlock(Block):
     max_value = 100
 
-    def __init__(self, env: Environment, _: Probabilities) -> None:
-        self.var = env.create_new_var()
-        self.value = rd.randint(0, DefineBlock.max_value)
+    def __init__(self, env: Scope, _: Probabilities) -> None:
+        self.def_var = env.create_new_var(prefix="f")
+        self.condition = Condition(self.def_var, str(rd.randint(2, ForBlock.max_value)), "<=")
+        self.next_blocks: List[Block] = []
 
     def render(self, visitor: Visitor) -> None:
-        string = f"int {self.var} = {self.value};"
+        string = f"for (int {self.def_var} = 0; {self.condition.render()}; {self.def_var}++) {{"
         visitor.send(string)
+        for block in self.next_blocks:
+            block.render(visitor)
+        visitor.send("}")
 
 
 class EntryPointBlock(Block):
-    def __init__(self, env: Environment, probs: Probabilities):
+    def __init__(self, env: Scope, probs: Probabilities):
         self.env = env
         self.probs = probs
         self.next_blocks: List[Block] = []
@@ -159,17 +166,11 @@ class EntryPointBlock(Block):
 
 
 class Generator:
-    def __init__(self) -> None:
-        rd.seed(42)
-        self.env = Environment()
+    def __init__(self, random_seed: int, blocks_count: int, probs: Probabilities) -> None:
+        self.probs = probs
+        rd.seed(random_seed)
+        self.env = Scope(blocks_count)
         self.block_generation_functions = [self.__gen_definition, self.__gen_for, self.__gen_if, self.__gen_statement]
-        self.probs = Probabilities(
-            blocks_chanses={key: 100 for key in self.block_generation_functions},
-            operators_chanses={key: 100 for key in self.env.bin_operators},
-            conditions_chanses={key: 100 for key in self.env.condition_operators},
-            blocks_cut=(2, 10),
-        )
-        self.env.blocks_count = 5
 
     def render(self, visitor: Visitor) -> None:
         self.entry_point.render(visitor)
@@ -181,7 +182,7 @@ class Generator:
         self.entry_point.next_blocks.append(def_block)
         self.entry_point.next_blocks.extend(self.__gen_local(env_copy))
 
-    def __gen_local(self, env: Environment) -> List[Block]:
+    def __gen_local(self, env: Scope) -> List[Block]:
         env.blocks_count -= 1
         if env.blocks_count <= 0:
             return []
@@ -192,21 +193,21 @@ class Generator:
             gen_blocks.append(next_block)
         return gen_blocks
 
-    def __gen_for(self, env: Environment) -> ForBlock:
+    def __gen_for(self, env: Scope) -> ForBlock:
         env_copy = env.copy()
         block = ForBlock(env_copy, self.probs)
         block.next_blocks = self.__gen_local(env_copy)
         return block
 
-    def __gen_if(self, env: Environment) -> IfConditionBlock:
+    def __gen_if(self, env: Scope) -> IfConditionBlock:
         env_copy = env.copy()
         block = IfConditionBlock(env_copy, self.probs)
         block.then_blocks = self.__gen_local(env.copy())
         block.else_blocks = self.__gen_local(env.copy())
         return block
 
-    def __gen_statement(self, env: Environment) -> OperationBlock:
+    def __gen_statement(self, env: Scope) -> OperationBlock:
         return OperationBlock(env, self.probs)
 
-    def __gen_definition(self, env: Environment) -> DefineBlock:
+    def __gen_definition(self, env: Scope) -> DefineBlock:
         return DefineBlock(env, self.probs)
