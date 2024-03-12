6a0976b8a5cef96b367167cf8570c37ded1b65cc
Osogi <dragonmail135159@gmail.com>
2024-03-13 00:34:16 +0300

    refactor: add thread safety to BGBuilder

--- src/helpers/backGroundBuilder.py
+++ src/helpers/backGroundBuilder.py
@@ -23,23 +23,17 @@ ChanSignal: TypeAlias = CSignal.BuiltFile | CSignal.End
 
 
 class BGBuilder:
-    # TODO: rewrite it more thread safety
-
     def __init__(self, settings: Namespace, builder: Builder | None = None):
         self.settings = settings
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(self.settings.log_level)
 
+        self.builder_mutex = threading.Lock()
         if builder is None:
             builder = Builder(settings)
         self.builder = builder
 
-    def build(
-        self,
-        src_file: Path,
-        destination_file: Path,
-        additional_flags: list[str] | None = None,
-    ):
+    def _build(self, src_file: Path, destination_file: Path, additional_flags: list[str] | None = None):
         self.builder.build(src_file, destination_file, additional_flags)
 
     def _build_dir(
@@ -52,12 +46,19 @@ class BGBuilder:
         list_of_src_files = os.listdir(src_dir)
         destination_dir.mkdir(parents=True, exist_ok=True)
 
+        self.builder_mutex.acquire()
         for test_file in list_of_src_files:
             out_file = destination_dir.joinpath(f"{test_file}.out")
-            self.build(src_dir.joinpath(test_file), out_file, additional_flags)
+            self._build(src_dir.joinpath(test_file), out_file, additional_flags)
             out_channel.put(CSignal.BuiltFile(out_file))
+        self.builder_mutex.release()
 
         out_channel.put(CSignal.End())
+
+    def build(self, src_file: Path, destination_file: Path, additional_flags: list[str] | None = None):
+        self.builder_mutex.acquire()
+        self._build(src_file, destination_file, additional_flags)
+        self.builder_mutex.release()
 
     def build_dir(
         self,
