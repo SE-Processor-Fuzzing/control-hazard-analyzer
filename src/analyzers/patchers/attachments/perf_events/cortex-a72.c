6edfb1d6755313096ce0b7e59cf2964b946a1cb4
Osogi <dragonmail135159@gmail.com>
2024-03-14 18:09:19 +0300

    feat: add raspberry events

---
+++ src/analyzers/patchers/attachments/perf_events/cortex-a72.c
@@ -0,0 +1,17 @@
+#include <linux/perf_event.h>
+
+typedef struct _perf_event_config_t {
+    unsigned int type;
+    unsigned long long config;
+    char* name;
+} perf_event_config_t;
+
+#define A72_PC_BRANCH_PRED 0x12
+
+perf_event_config_t events[] = {
+    //{PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS, "branches"},
+    {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES, "missed_branches"},
+    {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_BPU, "cache_BPU"},
+    {PERF_TYPE_HARDWARE, PERF_COUNT_SW_CPU_CLOCK, "cpu_clock"},
+    {PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, "instructions"},
+    {PERF_TYPE_RAW, A72_PC_BRANCH_PRED, "predicted_branches"}};
