4ebc469600430c4c033fcd901e4c189e462e1d40
Osogi <dragonmail135159@gmail.com>
2024-03-19 16:53:05 +0300

    refactor(aggregate): change analyzers configuration

--- src/cli/aggregate.py
+++ src/cli/aggregate.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 import logging
 import os
-import random
+from queue import Queue
 import shlex
 import shutil
 from argparse import ArgumentParser, Namespace
@@ -8,6 +9,7 @@ from pathlib import Path
 from pprint import pformat
 from typing import List
 
+from src.cli.analyze import Analyze
 from src.protocols.subparser import SubParser
 
 
@@ -15,6 +17,45 @@ class Aggregate:
     def __init__(self) -> None:
         self.settings = Namespace()
         self.logger = logging.getLogger(__name__)
+
+    # TODO: this is a draft method and should be removed or rewritted with asincio
+    def run_analyzers(self) -> List[str]:
+        output_analyze_dirs: List[str] = []
+
+        for analyze in self.analyzes:
+            analyze.run()
+            if analyze.settings is None:
+                raise LookupError("Didn't find settings for analyze")
+            output_analyze_dirs.append(analyze.settings.out_dir)
+
+        return output_analyze_dirs
+
+    def create_analyzer(self, config_file: Path, settings_analyze: Namespace) -> Analyze:
+        analyze = Analyze()
+        full_conf_path = Path(self.settings.path_to_configs).joinpath(config_file)
+        if os.access(full_conf_path, mode=os.R_OK):
+            cfg_settings = self.settings.configurator.read_cfg_file(full_conf_path)
+            settings_analyze = Namespace(
+                **self.settings.configurator.get_true_settings(
+                    self.settings.analyze.analyze_parser,
+                    cfg_settings,
+                    settings_analyze,
+                )
+            )
+            # if user set absolute path in config, we will save by it
+            if not Path(settings_analyze.out_dir).is_absolute():
+                name = full_conf_path.name.split(".")[0]
+                sub_dir_name = f"{name}-{datetime.now().strftime('%y-%m-%d-%H-%M-%S-%f')}"
+                sub_dir = Path(self.settings.dest_dir).joinpath(sub_dir_name)
+                while sub_dir.exists():
+                    sub_dir_name = sub_dir_name + "_new"
+                    sub_dir = sub_dir.with_name(sub_dir_name)
+                settings_analyze.out_dir = sub_dir.joinpath(settings_analyze.out_dir)
+
+            settings_analyze.log_level = self.settings.log_level
+            analyze.configurate(settings_analyze)
+
+        return analyze
 
     def clean_output_dir(self) -> None:
         shutil.rmtree(self.settings.dest_dir)
@@ -32,37 +73,14 @@ class Aggregate:
         self.settings.generate.run()
 
         # configure and run analyzer
-        self.output_analyze_dirs: List[str] = []
-
-        for config_file in self.settings.configs:
-            full_conf_path: Path = Path(self.settings.path_to_configs).joinpath(config_file)
-            if os.access(full_conf_path, mode=os.R_OK):
-                args_analyze = shlex.split(self.settings.Wz)
-                settings_analyze = self.settings.analyze.parse_args(args_analyze)
-                cfg_settings = self.settings.configurator.read_cfg_file(full_conf_path)
-                settings_analyze = Namespace(
-                    **self.settings.configurator.get_true_settings(
-                        self.settings.analyze.analyze_parser,
-                        cfg_settings,
-                        settings_analyze,
-                    )
-                )
-                sub_dir = f"{settings_analyze.profiler}-{random.getrandbits(16)}"
-                if not os.path.isabs(settings_analyze.out_dir):
-                    sub_dir = os.path.join(self.settings.dest_dir, sub_dir)
-                settings_analyze.out_dir = os.path.join(sub_dir, settings_analyze.out_dir)
-                settings_analyze.log_level = self.settings.log_level
-                self.settings.analyze.configurate(settings_analyze)
-                self.settings.analyze.run()
-
-                self.output_analyze_dirs.append(settings_analyze.out_dir)
+        self.output_analyze_dirs = self.run_analyzers()
 
         # configure and run summarizer
         args_summarize = shlex.split(self.settings.Ws)
         settings_summarize = self.settings.summarize.parse_args(args_summarize)
         settings_summarize.src_dirs = self.output_analyze_dirs
         settings_summarize.log_level = self.settings.log_level
-        settings_summarize.out_dir = os.path.join(self.settings.dest_dir, settings_summarize.out_dir)
+        settings_summarize.out_dir = Path(self.settings.dest_dir).joinpath(settings_summarize.out_dir)
 
         self.settings.summarize.configurate(settings_summarize)
         self.settings.summarize.run()
@@ -70,6 +88,11 @@ class Aggregate:
     def configurate(self, settings: Namespace) -> None:
         self.settings = Namespace(**{**vars(settings), **vars(self.settings)})
         self.logger.setLevel(self.settings.log_level)
+
+        # TODO: this is draft and should be rewritted
+        args_analyze = shlex.split(self.settings.Wz)
+        settings_analyze = self.settings.analyze.parse_args(args_analyze)
+        self.analyzes = [self.create_analyzer(cfg, settings_analyze) for cfg in self.settings.configs]
 
     def add_parser_arguments(self, subparser: SubParser) -> ArgumentParser:
         shell_parser: ArgumentParser = subparser.add_parser("aggregate", prog="aggregate")
