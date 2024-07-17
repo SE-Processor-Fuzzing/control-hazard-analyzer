07530b026817cf067dfca9d1620b6b40082811d9
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2024-07-18 00:26:00 +0300

    ci: add cache path parameter to install_gem5.py

--- scripts/install_gem5.py
+++ scripts/install_gem5.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 import sys
 
-from contextlib import chdir
 from pathlib import Path
 
 
@@ -17,10 +16,10 @@ variant_options = [
 ]
 
 help = f"""Usage:
-    python3 {sys.argv[0]} ISA VARIANT
+    python3 {sys.argv[0]} ISA VARIANT CACHE_PATH
     python3 {sys.argv[0]} {{help|-h|--help}}
 
-Configure and build gem5 for selected ISA with VARIANT optimization configuration.
+Configure and build gem5 for selected ISA with VARIANT optimization configuration and save build cache to CACHE PATH.
 You can read more about the gem5 build at https://www.gem5.org/documentation/general_docs/building.
 
 ISA available values:
@@ -30,8 +29,9 @@ VARIANT available values:
 {"\n".join([f"    - {var}" for var in variant_options])}
 
 Examples:
-    python3 {sys.argv[0]} X86 opt
-        Build gem5 for X86 isa with .opt gem5 configuration.
+    python3 {sys.argv[0]} X86 opt ./gem5_build_cache
+        Build gem5 for X86 isa with .opt gem5 configuration
+        and save build cache to ./gem5_build_cache directory.
     python3 {sys.argv[0]} help
         Print this help message.
 
@@ -42,35 +42,35 @@ root_dir = base_dir.parent
 configs_dir = root_dir.joinpath("configs")
 gem5_dir = root_dir.joinpath("thirdparty", "gem5")
 m5ops_dir = gem5_dir.joinpath("util", "m5")
-gem5_build_cache_dir = gem5_dir.joinpath("build_cache")
 
 
-def parse_args(args: list[str]) -> tuple[str, str]:
-    if len(sys.argv) == 2 and sys.argv[1] in help_options:
+def parse_args(args: list[str]) -> tuple[str, str, Path]:
+    if len(args) == 2 and args[1] in help_options:
         print(help)
         sys.exit(0)
-    if len(sys.argv) != 3:
+    if len(args) != 4:
         print("Illegal number of arguments")
         print(help)
         sys.exit(1)
-    isa_opt = sys.argv[1].upper()
+    isa_opt = args[1].upper()
     if isa_opt not in isa_options:
         print("Illegal ISA value: ")
         print(help)
         sys.exit(1)
-    variant_opt = sys.argv[2].lower()
+    variant_opt = args[2].lower()
     if variant_opt not in variant_options:
         print("Illegal VARIANT value")
         print(help)
         sys.exit(1)
-    return isa_opt, variant_opt
+    cache_path_opt = Path(args[3])
+    return isa_opt, variant_opt, cache_path_opt
 
 
-def build_gem5(isa: str, var: str) -> None:
+def build_gem5(isa: str, var: str, cache_path: Path) -> None:
     nproc = os.cpu_count()
     # Build gem5 with cache
     subprocess.run(["scons", "defconfig", f"build/{isa}", f"build_opts/{isa}", "--install-hooks"], cwd=gem5_dir)
-    subprocess.run(["scons", "setconfig", f"build/{isa}", f"M5_BUILD_CACHE={gem5_build_cache_dir}"], cwd=gem5_dir)
+    subprocess.run(["scons", "setconfig", f"build/{isa}", f"M5_BUILD_CACHE={cache_path}"], cwd=gem5_dir)
     subprocess.run(["scons", f"build/{isa}/gem5.{var}", "-j", f"{nproc}"], cwd=gem5_dir)
     # Build M5 ops
     m5_isa = isa.lower()
@@ -78,5 +78,5 @@ def build_gem5(isa: str, var: str) -> None:
 
 
 if __name__ == "__main__":
-    isa, var = parse_args(sys.argv)
-    build_gem5(isa, var)
+    isa, var, cache_path = parse_args(sys.argv)
+    build_gem5(isa, var, cache_path)
