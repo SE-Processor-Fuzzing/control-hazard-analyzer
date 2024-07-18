2d7b6c3034869f246b7cb1195a99870813eff118
VersusXX <sultanovmmt@gmail.com>
2024-07-18 04:44:43 +0300

    feat: change Namespace to Dict in helpers

--- src/helpers/configurator.py
+++ src/helpers/configurator.py
@@ -1,40 +1,15 @@
 import json
-from argparse import ArgumentParser, Namespace
-from typing import Any, Dict, Optional
-
-from src.protocols.utility import Utility
+from typing import Any, Dict, List
 
 
 class Configurator:
-    def __init__(self) -> None:
-        self.args: Optional[Namespace] = None
+    def configurate(self, args: Dict[str, Any]):
+        config_file = args.get("config_file")
+        section_in_config = args.get("section_in_config")
 
-        self.parser = ArgumentParser(
-            description="This script generate and test code on some platforms",
-        )
-
-        self.sub_parser = self.parser.add_subparsers(
-            required=True,
-            title="analyze and summarize",
-            help="aggregate is shell above: analyze (code generator + profiler), summarize",
-            dest="name_of_subparsers",
-            metavar="utility {aggregate, generate, analyze, summarize}",
-        )
-
-    def parse_sub_parsers(self, utilities: Dict[str, Utility]) -> tuple[Namespace, list[str]]:
-        for name, utility in utilities.items():
-            utility.add_parser_arguments(self.sub_parser).set_defaults(utility=name)
-
-        return self.parser.parse_known_args()
-
-    def configurate(self, utilities: Dict[str, Utility]) -> Namespace:
-        self.args = self.parse_sub_parsers(utilities)[0]
-        config = self.read_cfg_file(
-            getattr(self.args, "config_file", None),
-            self.args.section_in_config if hasattr(self.args, "section_in_config") else None,
-        )
-
-        return Namespace(**{**vars(self.args), **utilities, **config})
+        if config_file:
+            config = self.read_cfg_file(config_file, section_in_config)
+            args.update(config)
 
     def read_cfg_file(self, config_file: str | None, section: str | None = None) -> Dict[str, Any]:
         if config_file is None:
@@ -43,13 +18,21 @@ class Configurator:
             config: Dict[str, Any] = json.load(f)
         return config if section is None else {**config["DEFAULT"], **config[section]}
 
-    def get_true_settings(self, parser: ArgumentParser, settings: Dict[str, Any], args: Namespace) -> Dict[str, Any]:
+    def get_true_settings(self, settings: Dict[str, Any], args: Dict[str, Any]) -> Dict[str, Any]:
         result: Dict[str, Any] = settings
-        dct_args = {**vars(args)}
-        for arg in dct_args:
+        for arg in args:
             if arg in settings:
-                if dct_args[arg] != parser.get_default(arg):
-                    settings[arg] = dct_args[arg]
+                if args[arg] != settings[arg]:
+                    settings[arg] = args[arg]
             else:
-                settings[arg] = dct_args[arg]
+                settings[arg] = args[arg]
         return result
+
+    def parse_args(self, args: List[str], default_params: Dict[str, Any]) -> Dict[str, Any]:
+        for i in range(0, len(args), 2):
+            key = args[i].lstrip("--").replace("-", "_")
+            value = args[i + 1]
+            if key in default_params:
+                default_params[key] = value
+
+        return default_params
