45efbd43e82742857962fd66b1d00d3d7e514f0c
VersusXX <sultanovmmt@gmail.com>
2024-07-18 04:35:39 +0300

    feat: change Namespace to Dict in utilities

--- src/cli/generate.py
+++ src/cli/generate.py
@@ -1,26 +1,24 @@
 import logging
 import shutil
-from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from pprint import pformat
-from typing import List, Optional
-
+from typing import Optional, Any, Dict
 from src.generators.code_gen import gen_test
-from src.protocols.subparser import SubParser
+from src.protocols.utility import Utility
 
 
-class Generate:
+class Generate(Utility):
     def __init__(self) -> None:
-        self.settings: Optional[Namespace] = None
+        self.settings: Optional[Dict[str, Any]] = None
         self.repeats: Optional[int] = None
         self.out_dir: Optional[Path] = None
         self.logger = logging.getLogger(__name__)
 
-    def configurate(self, settings: Namespace) -> None:
+    def configurate(self, settings: Dict[str, Any]) -> None:
         self.settings = settings
-        self.logger.setLevel(self.settings.log_level)
-        self.out_dir = Path(settings.out_dir)
-        self.repeats = int(settings.repeats)
+        self.logger.setLevel(self.settings["log_level"])
+        self.out_dir = Path(settings["out_dir"])
+        self.repeats = int(settings["repeats"])
 
     def generate_tests(
         self,
@@ -35,7 +33,7 @@ class Generate:
     def _generate_test(self, file: Path, max_depth: int) -> None:
         test = gen_test(max_depth)
         if file.is_dir():
-            self.logger.warn(f"Provided path {file} are not a file. Skipping this test.")
+            self.logger.warn(f"Provided path {file} is not a file. Skipping this test.")
             return
         with open(file, "w") as f:
             self.logger.info(f"Write test into {file}")
@@ -48,25 +46,16 @@ class Generate:
 
     def run(self) -> None:
         self.logger.info("Generate is running. Settings:")
-        self.logger.info(pformat(vars(self.settings)))
+        self.logger.info(pformat(self.settings))
         if self.out_dir is None or self.repeats is None:
             self.logger.warn("Out dir or repeats values are not provided. Exiting...")
             return
         self.create_empty_dir(self.out_dir)
         self.generate_tests(self.out_dir, self.repeats)
 
-    def add_parser_arguments(self, subparser: SubParser) -> ArgumentParser:
-        generate_parser = subparser.add_parser("generate")
-        generate_parser.add_argument("--out-dir", default="tests", help="Path to output dir")
-        generate_parser.add_argument("--repeats", type=int, default=1, help="Count of repeats to generated test")
-        generate_parser.add_argument(
-            "--log-level",
-            default="WARNING",
-            choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-            help="Log level of program",
-        )
-        self.generate_parser = generate_parser
-        return generate_parser
-
-    def parse_args(self, args: List[str]) -> Namespace:
-        return self.generate_parser.parse_known_args(args)[0]
+    default_params = {
+        "utility": "generate",
+        "out_dir": "tests",
+        "repeats": 1,
+        "log_level": "WARNING"
+    }
