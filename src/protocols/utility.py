22b2bdf99147ad321cd3edbe081390bef5544321
VersusXX <sultanovmmt@gmail.com>
2024-07-18 04:34:14 +0300

    feat: change Utility structure

--- src/protocols/utility.py
+++ src/protocols/utility.py
@@ -1,14 +1,7 @@
-from argparse import ArgumentParser, Namespace
-from typing import List, Protocol
-
-from src.protocols.subparser import SubParser
+from typing import Protocol, Any, Dict
 
 
 class Utility(Protocol):
     def run(self) -> None: ...
 
-    def configurate(self, settings: Namespace) -> None: ...
-
-    def add_parser_arguments(self, subparser: SubParser) -> ArgumentParser: ...
-
-    def parse_args(self, args: List[str]) -> Namespace: ...
+    def configurate(self, settings: Dict[str, Any]) -> None: ...
